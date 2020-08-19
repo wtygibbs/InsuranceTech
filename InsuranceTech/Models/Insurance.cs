9a30620d02f906555379d167c54ea86f902c5456
wtylergibbons <pHQM6n5CxK7vJK>
2020-08-18 23:34:09 -0400

    Basic insurance model, member function to calculate premium.

---
+++ InsuranceTech/Models/Insurance.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceTech.Models
+{ 
+    public class Insurance
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Primary Address")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Secondary Address")]
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Effective Date")]
+        [DataType(DataType.Date)]
+        public DateTime EffectiveDate { get; set; }
+
+        [Display(Name = "Expiration Date")]
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+
+        [Display(Name = "Total Insured Value")]
+        [DisplayFormat(DataFormatString = "{0:$0.00}")]
+        public decimal TotalInsuredValue { get; set; }
+
+        [Display(Name = "Business Type")]
+        public EnumBusiness Business { get; set; }
+
+        public enum EnumBusiness
+        {
+            Corporation,
+            Partnership,
+            LimitedLiabilityCompany,
+            LLC
+        }
+
+        public decimal Rate { get; set; } = 1.0M;
+
+        [DisplayFormat(DataFormatString = "{0:$0.00}")]
+        public decimal Premium { get; set; }
+
+        public Insurance()
+        { 
+        }
+
+        public decimal calculatePremium(string st, decimal tiv, decimal r)
+        {
+            if(st.ToUpper().Equals("FL") || st.ToUpper().Equals("FLORIDA"))
+            {
+                return (tiv * (r + 0.07M)) / 100;
+            }
+
+            return (tiv * r) / 100;
+        }
+    }
+}
